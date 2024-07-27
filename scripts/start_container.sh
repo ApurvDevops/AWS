2b116638af990a99e674cf8c1328130486690609
Apurv Samadder <83865632+ApurvDevops@users.noreply.github.com>
2024-07-27 14:39:18 +0200

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull testloginapurv/simple-python-flask-app:latest
+docker pull testloginapurv/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000
+docker run -d -p 5000:5000 testloginapurv/simple-python-flask-app
