b8cc10f10bbf662fb75de15e8aeb08b592aee4d3
ApurvDevops <apurv.samadder@gmail.com>
2024-07-31 09:03:23 +0200

    Updated python file

--- simple-python-app/app.py
+++ simple-python-app/app.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 def hello():
     return 'Hello, world!I succded in Pushing to Docker'
     return 'This is Code Pipeline'
+    return 'This is code Pipeline working'
 
 if __name__ == '__main__':
     app.run()
